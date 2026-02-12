7ea9706f2426c6f328860d53f6a079fa202d1096
naoki koike <annynaoki@gmail.com>
2026-02-12 09:20:32 -0500

    setting up

--- projects/portrait/portrait.js
+++ projects/portrait/portrait.js
@@ -1,2 +1,39 @@
+	//draw rect	
+//		ctx.fillStyle = "green";
+//		ctx.fillRect(100, 100, 200, 300);
+	//draw circle
+//		ctx.fillStyle = "red";
+//		ctx.beginPath();
+//		ctx.arc(400, 500, 100, 500, Math.PI**2);
+//		ctx.fill();
+	//draw elipse
+//		ctx.fillStyle = "black";
+//		ctx.beginPath();
+//		ctx.ellipse(300, 700, 200, 50, 0, Math.PI/6, Math.PI**2);
+//		ctx.fill();	
+	//draw triangle		
+//		ctx.beginPath();
+//		ctx.moveTo(700, 50);
+//		ctx.lineTo(750, 150);
+//		ctx.lineTo(650, 150);
+//		ctx.stroke();
+//		ctx.fill();
+
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");     
+
+//draw outline
+
+//draw head
+ctx.beginPath();
+ctx.moveTo(700, 100);
+ctx.lineTo(550, 30);
+ctx.lineTo(650, 150);
+//draw eyes
+
+//draw nose
+
+//draw ears
+
+//mouth
