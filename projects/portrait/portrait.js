d4c3c476be1f4ff6e661fb3e6d45a171722d1fcd
naoki koike <annynaoki@gmail.com>
2026-02-14 17:17:50 -0500

    fixing error and starting project

--- projects/portrait/portrait.js
+++ projects/portrait/portrait.js
@@ -24,12 +24,48 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");     
 
 //draw outline
+ctx.beginPath();
+ctx.moveTo(150, 350);
+ctx.lineTo(200, 200);
+ctx.lineTo(550 ,330);
+ctx.lineTo(550 ,450);
+ctx.lineTo(500 ,550);
+ctx.lineTo(450 ,600);
+ctx.lineTo(370 ,650);
+ctx.lineTo(300 ,650);
+ctx.lineTo(240 ,600);
+ctx.lineTo(180 ,500);
+ctx.fillStyle = "#FFBF00";
+ctx.fill();
 
 //draw head
 ctx.beginPath();
-ctx.moveTo(700, 100);
-ctx.lineTo(550, 30);
-ctx.lineTo(650, 150);
+ctx.moveTo(130, 170);
+ctx.lineTo(250, 80);
+ctx.lineTo(150, 350);
+ctx.fillStyle = "black";
+ctx.fill();
+
+ctx.beginPath();
+ctx.ellipse(400, 200, 250, 150, 0, Math.PI, Math.PI*2);
+ctx.fillStyle = "black";
+ctx.fill();	
+
+ctx.beginPath();
+ctx.moveTo(200, 200);
+ctx.lineTo(550, 330);
+ctx.lineTo(550, 150);
+ctx.fillStyle = "black";
+ctx.fill();
+
+ctx.fillStyle = "black";
+ctx.fillRect(550, 200, 100, 130);
+
+ctx.beginPath();
+ctx.moveTo(550, 330);
+ctx.lineTo(550, 450);
+ctx.lineTo(650, 330);
+ctx.fill();
 //draw eyes
 
 //draw nose
